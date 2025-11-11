47629ef0c0cd72c56aa667be0ef479ab308992fe
jd115461 <hpreston@wawarra.com>
2025-11-11 10:18:53 +1000

    Add completed product class referencing the products name and price.

--- product.py
+++ product.py
@@ -1,1 +1,17 @@
-""""""
+"""
+CP1404 Seminar task
+Create a product class
+"""
+
+
+class Product:
+    """Class for storing detail of a product"""
+
+    def __init__(self, name="", price=0):
+        """Initialise a product"""
+        self.name = name
+        self.price = price
+
+    def __str__(self):
+        """Return a string representation of a product"""
+        return f"{self.name} : ${self.price:,.2f}"
